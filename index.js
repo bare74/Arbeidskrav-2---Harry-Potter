0bc85f0bf889bcfc2ac9b7b4e742776dd7d6ea91
mobak88 <mostafa0401@ymail.com>
2022-03-30 22:03:24 +0200

    Making each student card have random bg color

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const API = 'http://hp-api.herokuapp.com/api/characters';
 const severusData = [];
 const students = [];
 
+const generateStudentsBtn = document.querySelector('.start-class-btn');
+
 const severus = document.getElementById('severus-card');
 const severusImgContainer = document.createElement('div');
 const severusIgm = document.createElement('img');
@@ -10,20 +12,39 @@ const severusAge = document.createElement('p');
 severus.append(severusImgContainer, severusName, severusAge);
 severusImgContainer.append(severusIgm);
 
+function clearItems(el) {
+    el.innerHTML = '';
+}
+
 function generateRandomStudents(arr) {
     const pupilsContainer = document.querySelector('.pupils-container');
 
+    clearItems(pupilsContainer);
+
     for (let i = 0; i <= 9; i++) {
         const randomNumber = Math.round(Math.random() * 101);
+
         if (arr.indexOf(randomNumber) === -1) {
             console.log(arr[randomNumber]);
             pupilsContainer.innerHTML += `
                 <div class='student-container'>
                     <img class='student-img' src=${arr[randomNumber].image ? arr[randomNumber].image : "./assets/avatar.png"} />
-                    <p>${arr[randomNumber].name}</p>
-                    <p>${arr[randomNumber].house}</p>
+                    <p>Navn: ${arr[randomNumber].name}</p>
+                    <p>Hus: ${arr[randomNumber].house}</p>
+                    <button class='delete-student-btn'>Slett Elev</button>
                 </div>
             `;
+
+            document.querySelectorAll('.student-container').forEach(background => {
+                const x = Math.floor(Math.random() * 100 + 80);
+                const y = Math.floor(Math.random() * 100 + 80);
+                const z = Math.floor(Math.random() * 100 + 80);
+                const bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+                console.log(bgColor);
+
+                background.style.background = bgColor;
+            });
         }
     }
 }
@@ -47,7 +68,10 @@ async function fetchData() {
         severusAge.textContent = `Alder: ${2022 - severusData[0].yearOfBirth
             } år`;
 
-        generateRandomStudents(students);
+
+        generateStudentsBtn.addEventListener('click', () => {
+            generateRandomStudents(students);
+        });
     }
     catch (err) {
         console.log(err);
@@ -68,3 +92,4 @@ function hideBubble() {
 
 severusImgContainer.addEventListener('mouseenter', showBubble);
 severus.addEventListener('mouseleave', hideBubble);
+
