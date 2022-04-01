95259528213a9c4b06d3514dda06b2b6298dd256
mobak88 <mostafa0401@ymail.com>
2022-04-01 20:19:14 +0200

    Changing aproach to make sure each student is unique and changing styling to align with the rest of the page

--- script/potions-class.js
+++ script/potions-class.js
@@ -1,6 +1,7 @@
 const API = 'http://hp-api.herokuapp.com/api/characters';
 const severusData = [];
 const students = [];
+let uniqueStudents = [];
 
 const generateStudentsBtn = document.querySelector('.start-class-btn');
 
@@ -16,7 +17,7 @@ function clearItems(el) {
     el.innerHTML = '';
 }
 
-function createStudent(arr, i) {
+function createNewStudent(arr, i) {
     const studentsActive = document.querySelectorAll('.student-container');
     const userAnswer = prompt('Ønsker du å slette? Skriv ja/nei');
 
@@ -39,38 +40,48 @@ function deleteStudent(deleteBtn) {
     deleteBtn.forEach((btn, i) => {
         btn.addEventListener('click', () => {
             randomNumber = Math.round(Math.random() * 101);
-            createStudent(students, i);
+            createNewStudent(students, i);
         });
     });
 }
 
-function generateRandomStudents(arr) {
+function generateRandomStudents() {
+    const studentsSet = new Set();
+
+    while (studentsSet.size < 10) {
+        let randomNumber = Math.round(Math.random() * 101);
+        studentsSet.add(randomNumber);
+    }
+
+    uniqueStudents = [...studentsSet];
+    console.log(uniqueStudents);
+}
+
+function createStudentsCards() {
     const pupilsContainer = document.querySelector('.pupils-container');
 
     clearItems(pupilsContainer);
 
-    for (let i = 0; i <= 9; i++) {
-        let randomNumber = Math.round(Math.random() * 101);
+    generateRandomStudents();
 
-        if (arr.indexOf(randomNumber) === -1) {
-            pupilsContainer.innerHTML += `
+    uniqueStudents.forEach(student => {
+        pupilsContainer.innerHTML += `
                 <div class='student-container'>
-                <img class='student-img' src=${!arr[randomNumber].image ? "./assets/avatar.png" : arr[randomNumber].image} />
-                <p>Navn: ${arr[randomNumber].name}</p>
-                <p>Hus: ${arr[randomNumber].house ? arr[randomNumber].house : 'Intet hus'}</p>
+                <img class='student-img' src=${!students[student].image ? "./assets/avatar.png" : students[student].image} />
+                <p>Navn: ${students[student].name}</p>
+                <p>Hus: ${students[student].house ? students[student].house : 'Intet hus'}</p>
                 <button class='delete-student-btn'>Slett Elev</button>
                 </div>
                 `;
 
-            document.querySelectorAll('.student-container').forEach(background => {
-                const x = Math.floor(Math.random() * 100 + 80);
-                const y = Math.floor(Math.random() * 100 + 80);
-                const z = Math.floor(Math.random() * 100 + 80);
-                const bgColor = "rgb(" + x + "," + y + "," + z + ")";
-                background.style.background = bgColor;
-            });
-        }
-    }
+        document.querySelectorAll('.student-container').forEach(background => {
+            const x = Math.floor(Math.random() * 100 + 80);
+            const y = Math.floor(Math.random() * 100 + 80);
+            const z = Math.floor(Math.random() * 100 + 80);
+            const bgColor = "rgb(" + x + "," + y + "," + z + ")";
+            background.style.background = bgColor;
+        });
+    });
 
     const deleteStudentBtn = document.querySelectorAll('.delete-student-btn');
 
@@ -98,7 +109,7 @@ async function fetchData() {
         generateStudentsBtn.addEventListener('click', () => {
             const pupilsContainer = document.querySelector('.pupils-container');
             pupilsContainer.classList.remove('container-hidden');
-            generateRandomStudents(students);
+            createStudentsCards();
         });
 
     }
@@ -114,6 +125,7 @@ function showBubble() {
     talkingBubble.classList.remove('container-hidden');
 }
 
+
 function hideBubble() {
     const talkingBubble = document.getElementById('talking-bubble-container');
     talkingBubble.classList.add('container-hidden');
@@ -121,4 +133,3 @@ function hideBubble() {
 
 severusImgContainer.addEventListener('mouseenter', showBubble);
 severusImgContainer.addEventListener('mouseleave', hideBubble);
-
