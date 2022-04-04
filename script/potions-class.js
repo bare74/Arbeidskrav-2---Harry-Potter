9a04f0b106e3f76c368563ab1e7f12a8fd1f94bb
bare74 <82520963+bare74@users.noreply.github.com>
2022-04-04 21:06:32 +0200

    delet student

--- script/potions-class.js
+++ script/potions-class.js
@@ -1,146 +1,167 @@
-import { API } from '../script/index.js'
+import { API } from "../script/index.js";
 const severusData = [];
 const students = [];
 let uniqueStudents = [];
 
-const generateStudentsBtn = document.querySelector('.start-class-btn');
+const generateStudentsBtn = document.querySelector(".start-class-btn");
 
-const severus = document.getElementById('severus-card');
-const severusImgContainer = document.createElement('div');
-const severusIgm = document.createElement('img');
-const severusName = document.createElement('p');
-const severusAge = document.createElement('p');
+const severus = document.getElementById("severus-card");
+const severusImgContainer = document.createElement("div");
+const severusIgm = document.createElement("img");
+const severusName = document.createElement("p");
+const severusAge = document.createElement("p");
 severus.append(severusImgContainer, severusName, severusAge);
 severusImgContainer.append(severusIgm);
 
 function clearItems(el) {
-    el.innerHTML = '';
+  el.innerHTML = "";
 }
 
 function createNewStudent(arr, i) {
-    const studentsActive = document.querySelectorAll('.student-container');
-    const userAnswer = prompt('Ønsker du å slette? Skriv ja/nei');
+  const studentsActive = document.querySelectorAll(".student-container");
+  const userAnswer = prompt("Ønsker du å slette? Skriv ja/nei");
 
-    if (userAnswer === null || userAnswer.toLowerCase() !== 'ja') {
-        return;
-    } else {
-        studentsActive[i].innerHTML = `
-            <img class='student-img' alt='${!arr[randomNumber].image ? 'Missing image avatar' : arr[randomNumber].name + ' image'}' image' src=${!arr[randomNumber].image ? "./assets/avatar.png" : arr[randomNumber].image} />
+  if (userAnswer === null || userAnswer.toLowerCase() !== "ja") {
+    return;
+  } else {
+    const randomNumber = Math.round(Math.random() * (students.length - 1));
+    studentsActive[i].innerHTML = `
+            <img class='student-img' alt='${
+              !arr[randomNumber].image
+                ? "Missing image avatar"
+                : arr[randomNumber].name + " image"
+            }' image' src=${
+      !arr[randomNumber].image ? "./assets/avatar.png" : arr[randomNumber].image
+    } />
             <p>Navn: ${arr[randomNumber].name}</p>
-            <p>Hus: ${arr[randomNumber].house ? arr[randomNumber].house : 'Intet hus'}</p>
+            <p>Hus: ${
+              arr[randomNumber].house ? arr[randomNumber].house : "Intet hus"
+            }</p>
             <button class='delete-student-btn'>Slett elev</button>
         `;
 
-        const deleteStudentBtn = document.querySelectorAll('.delete-student-btn');
-        deleteStudent(deleteStudentBtn);
-    }
+    const deleteStudentBtn = document.querySelectorAll(".delete-student-btn");
+    deleteStudent(deleteStudentBtn);
+  }
 }
 
 function deleteStudent(deleteBtn) {
-    deleteBtn.forEach((btn, i) => {
-        btn.addEventListener('click', () => {
-            randomNumber = Math.round(Math.random() * (students.length - 1));
-            createNewStudent(students, i);
-        });
+  deleteBtn.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+      createNewStudent(students, i);
     });
+  });
 }
 
 function generateRandomStudents() {
-    const studentsSet = new Set();
+  const studentsSet = new Set();
 
-    while (studentsSet.size < 10) {
-        let randomNumber = Math.round(Math.random() * (students.length - 1));
-        studentsSet.add(randomNumber);
-    }
+  while (studentsSet.size < 10) {
+    let randomNumber = Math.round(Math.random() * (students.length - 1));
+    studentsSet.add(randomNumber);
+  }
 
-    uniqueStudents = [...studentsSet];
-    console.log(uniqueStudents);
+  uniqueStudents = [...studentsSet];
+  console.log(uniqueStudents);
 }
 
 function generateRandomColors(list) {
-    list.forEach(background => {
-        const bgColor = "hsl(" + 360 * Math.random() + ',' +
-            (25 + 70 * Math.random()) + '%,' +
-            (85 + 10 * Math.random()) + '%)';
-        background.style.background = bgColor;
-    });
+  list.forEach((background) => {
+    const bgColor =
+      "hsl(" +
+      360 * Math.random() +
+      "," +
+      (25 + 70 * Math.random()) +
+      "%," +
+      (85 + 10 * Math.random()) +
+      "%)";
+    background.style.background = bgColor;
+  });
 }
 
 function createStudentsCards() {
-    const pupilsContainer = document.querySelector('.pupils-container');
+  const pupilsContainer = document.querySelector(".pupils-container");
 
-    clearItems(pupilsContainer);
+  clearItems(pupilsContainer);
 
-    generateRandomStudents();
+  generateRandomStudents();
 
-    uniqueStudents.forEach(student => {
-        pupilsContainer.innerHTML += `
+  uniqueStudents.forEach((student) => {
+    pupilsContainer.innerHTML += `
             <li class='student-container'>
-                <img class='student-img' alt='${!students[student].image ? 'Missing image avatar' : students[student].name + ' image'}' src=${!students[student].image ? "./assets/avatar.png" : students[student].image} />
+                <img class='student-img' alt='${
+                  !students[student].image
+                    ? "Missing image avatar"
+                    : students[student].name + " image"
+                }' src=${
+      !students[student].image ? "./assets/avatar.png" : students[student].image
+    } />
                 <p>Navn: ${students[student].name}</p>
-                <p>Hus: ${students[student].house ? students[student].house : 'Intet hus'}</p>
+                <p>Hus: ${
+                  students[student].house
+                    ? students[student].house
+                    : "Intet hus"
+                }</p>
                 <button class='delete-student-btn'>Slett elev</button>
             </li>
             `;
 
-        const studentsActive = document.querySelectorAll('.student-container');
+    const studentsActive = document.querySelectorAll(".student-container");
 
-        generateRandomColors(studentsActive);
-    });
+    generateRandomColors(studentsActive);
+  });
 
-    const deleteStudentBtn = document.querySelectorAll('.delete-student-btn');
+  const deleteStudentBtn = document.querySelectorAll(".delete-student-btn");
 
-    deleteStudent(deleteStudentBtn);
+  deleteStudent(deleteStudentBtn);
 }
 
 function filterStudents(data) {
-    data.filter((student) => {
-        if (student.hogwartsStudent === true) {
-            students.push(student);
-        }
-    });
+  data.filter((student) => {
+    if (student.hogwartsStudent === true) {
+      students.push(student);
+    }
+  });
 }
 
 function displayTeacherDetails(data) {
-    severusIgm.src = data[0].image;
-    severusName.textContent = 'Navn: ' + data[0].name;
-    severusAge.textContent = `Alder: ${2022 - data[0].yearOfBirth
-        } år`;
+  severusIgm.src = data[0].image;
+  severusName.textContent = "Navn: " + data[0].name;
+  severusAge.textContent = `Alder: ${2022 - data[0].yearOfBirth} år`;
 }
 
 function displayStudentsHandler() {
-    generateStudentsBtn.addEventListener('click', () => {
-        const pupilsContainer = document.querySelector('.pupils-container');
-        pupilsContainer.classList.remove('container-hidden');
-        createStudentsCards();
-    });
+  generateStudentsBtn.addEventListener("click", () => {
+    const pupilsContainer = document.querySelector(".pupils-container");
+    pupilsContainer.classList.remove("container-hidden");
+    createStudentsCards();
+  });
 }
 
 async function fetchData() {
-    try {
-        const response = await fetch(API);
-        const data = await response.json();
-        severusData.push(data[7]);
-        filterStudents(data);
-        displayTeacherDetails(severusData);
-        displayStudentsHandler();
-    }
-    catch (err) {
-        console.log(err);
-    }
+  try {
+    const response = await fetch(API);
+    const data = await response.json();
+    severusData.push(data[7]);
+    filterStudents(data);
+    displayTeacherDetails(severusData);
+    displayStudentsHandler();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 fetchData();
 
 function showBubble() {
-    const talkingBubble = document.getElementById('talking-bubble-container');
-    talkingBubble.classList.remove('container-hidden');
+  const talkingBubble = document.getElementById("talking-bubble-container");
+  talkingBubble.classList.remove("container-hidden");
 }
 
 function hideBubble() {
-    const talkingBubble = document.getElementById('talking-bubble-container');
-    talkingBubble.classList.add('container-hidden');
+  const talkingBubble = document.getElementById("talking-bubble-container");
+  talkingBubble.classList.add("container-hidden");
 }
 
-severusImgContainer.addEventListener('mouseenter', showBubble);
-severusImgContainer.addEventListener('mouseleave', hideBubble);
+severusImgContainer.addEventListener("mouseenter", showBubble);
+severusImgContainer.addEventListener("mouseleave", hideBubble);
